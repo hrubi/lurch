945949a4d826890e29698524c6dd49da170584c3
Jan Hruban <jan.hruban@gooddata.com>
2014-07-30 00:28:18 +0200

    Add sanity checks for driver path

--- src/lurch_device_driver.erl
+++ src/lurch_device_driver.erl
@@ -13,4 +13,33 @@ start_driver( _Driver, _Parameters ) ->
 
 
 driver_path( Driver ) ->
-	filename:join( [ code:lib_dir( lurch, drivers ), Driver ] ).
+	case filename:pathtype(Driver) of
+		relative -> ok;
+		_ -> throw({error, bad_driver})
+	end,
+	case lists:member("..", filename:split(Driver)) of
+		true -> throw({error, bad_driver});
+		false -> ok
+	end,
+	filename:join( [ driver_dir( ), Driver ] ).
+
+driver_dir( ) ->
+	code:lib_dir( lurch, drivers ).
+
+
+
+%% ===================================================================
+%% Tests
+%% ===================================================================
+-ifdef( TEST ).
+-include_lib( "eunit/include/eunit.hrl" ).
+
+driver_path_test_( ) ->
+	{ "Driver filename sanity checks",
+		[ ?_assertThrow({error, bad_driver}, driver_path("/absolute/path"))
+		, ?_assertThrow({error, bad_driver}, driver_path("dots/../in/path"))
+		, ?_assertThrow({error, bad_driver}, driver_path("dots/end/.."))
+		]
+	}.
+
+-endif. % TEST
